ca09a7dd258da08da6fdd76bbce19c911f28abb6
Vershinin AI <thealexmaps@yandex.ru>
2025-06-21 03:20:13 +0700

    fp

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.pAGES;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public versh_dbEntities db = new versh_dbEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            Page1 nextPage = new Page1();
+            frame.Navigate(nextPage);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ pAGES/list.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1.pAGES
+{
+    /// <summary>
+    /// Логика взаимодействия для list.xaml
+    /// </summary>
+    public partial class list : Page
+    {
+        public ObservableCollection<Tovari> Tv = new ObservableCollection<Tovari>();
+        public versh_dbEntities db= new versh_dbEntities();
+        public list()
+        {
+        
+            InitializeComponent();
+            this.DataContext = this;
+            foreach (Tovari Itm in db.Tovari.ToList())
+            {
+                Tv.Add(Itm);
+                
+            }
+            lbList.ItemsSource = Tv;
+        }
+    }
+}
